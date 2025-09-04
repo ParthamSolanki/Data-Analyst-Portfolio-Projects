fa0b93172d88b72f16bceabfd2285e0815ec17af
Parth <121657963+ParthamSolanki@users.noreply.github.com>
2025-09-04 08:39:17 +0530

    Framework of EDA SQL

--- SQL/EDA_books.sql
+++ SQL/EDA_books.sql
@@ -4,3 +4,11 @@ SELECT * OVER (PARTITION BY authors)
 FROM books_cleaned
 ORDER BY average_rating DESC
 LIMIT 5;
+
+# Here we are going to get summary stats of the whole cleaned dataset.
+# Popularity of books over a certain ratings or view count to keep the numbers more concise and avoid spam.
+# Time based analysis
+# Top books based on the year of release to get a by year rating.
+# Average ratings of books by year
+# Publisher performance by finding how many decent performing books a publisher has under their belt.
+# Quantity of books based on language.
